caffbbf539e1a46e3ccff40f7638710d2a0709d9
Marco Piatti <marco.piatti@despegar.com>
2025-09-21 21:57:36 -0300

    optimized imports

--- store-stock-update/src/main/scala/service/StockUpdater.scala
+++ store-stock-update/src/main/scala/service/StockUpdater.scala
@@ -1,16 +1,14 @@
 package ims.store
 package service
 
+import api.ApiError
 import db.Queries
 import domain.StockUpdate
 
 import cats.effect.IO
-import cats.free.Free
 import cats.implicits.catsSyntaxApplicativeId
 import doobie.*
 import doobie.implicits.*
-import cats.syntax.*
-import ims.store.api.ApiError
 
 trait StockUpdater:
   def apply(stock: StockUpdate): IO[Either[ApiError, StockUpdate]]
@@ -21,7 +19,7 @@ object StockUpdater:
       for
         quantity <- Queries.stock(stock.sku)
         newQuantity = quantity + stock.quantity
-        result <- if newQuantity >= 0 
+        result <- if newQuantity >= 0
                     then Queries.updateStock(storeId, stock.sku, newQuantity).map(Right(_))
                     else ApiError.of(s"Not enough stock. Quantity: $quantity, attempted removal: ${stock.quantity}").pure[ConnectionIO]
       yield result
