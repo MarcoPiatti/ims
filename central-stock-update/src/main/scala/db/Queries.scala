0b87a94c22f455ad66935bc409e124b9a3ca4cce
Marco Piatti <marco.piatti@despegar.com>
2025-09-23 05:10:28 -0300

    persist last update in stock

--- central-stock-update/src/main/scala/db/Queries.scala
+++ central-stock-update/src/main/scala/db/Queries.scala
@@ -5,11 +5,12 @@ import cats.implicits.toFunctorOps
 import cats.implicits.catsSyntaxApplicativeId
 import doobie.*
 import doobie.implicits.*
+import doobie.implicits.legacy.instant.JavaTimeInstantMeta
 
 import java.time.Instant
 
 object Queries {
-  def updateStock(storeId: Int, sku: String, id: Int, quantity: Int, createdAt: String): ConnectionIO[Unit] =
+  def updateStock(storeId: Int, sku: String, id: Int, quantity: Int, createdAt: Instant): ConnectionIO[Unit] =
     val query = for
       _ <- sql"""
         insert into transactions (sku, store_id, id, quantity, created_at)
@@ -17,8 +18,8 @@ object Queries {
       """.update.run
 
       _ <- sql"""
-        insert into stock (sku, store_id, quantity) values ($sku, $storeId, $quantity)
-        on duplicate key update quantity = VALUES(quantity)
+        insert into stock (sku, store_id, quantity, last_updated) values ($sku, $storeId, $quantity, $createdAt)
+        on duplicate key update quantity = VALUES(quantity) and last_updated = VALUES(last_updated)
       """.update.run
     yield ()
 
--- central-stock-update/src/main/scala/kafka/stockUpdate/StockUpdate.scala
+++ central-stock-update/src/main/scala/kafka/stockUpdate/StockUpdate.scala
@@ -1,5 +1,7 @@
 package ims.central.update
 package kafka.stockUpdate
 
+import java.time.Instant
+
 case class StockUpdateKey(store_id: Int, sku: String)
-case class StockUpdateData(id: Int, store_id: Int, sku: String, quantity: Int, created_at: String)
+case class StockUpdateData(id: Int, store_id: Int, sku: String, quantity: Int, created_at: Instant)
