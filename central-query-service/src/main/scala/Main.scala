6e3dc2b72cdce1169ef38b2f9f1cfd1036846591
Marco Piatti <marco.piatti@despegar.com>
2025-09-24 05:06:15 -0300

    initialized reservation result processing

--- central-query-service/src/main/scala/Main.scala
+++ central-query-service/src/main/scala/Main.scala
@@ -3,8 +3,8 @@ package ims.central.query
 import api.Api
 import config.Config
 import db.Db
-import kafka.Kafka
-import kafka.reservations.{ReservationData, ReservationKey}
+import kafka.{ChunkProcessor, Kafka}
+import kafka.reservations.{ReservationData, ReservationKey, ReservationResultProcessor}
 import server.{Routes, Server}
 import service.{ReservationPersistor, StockQueryService}
 
@@ -23,6 +23,8 @@ object Main extends IOApp.Simple:
       for
         transactor <- Db.transactor(config.db)
         kafkaSender <- Kafka.sender[ReservationKey, ReservationData](config.kafka.reservations)
+        reservationResultProcessor = ReservationResultProcessor(transactor)
+        _ <- Kafka.chunkConsumer(config.kafka.reservationResults, ChunkProcessor.all(reservationResultProcessor.process)).background
         stockQueryService = StockQueryService(config.reservation.minimumStock, config.store.syncLimit, transactor)
         reservationPersistor = ReservationPersistor(config.reservation.minimumStock, transactor, kafkaSender)
         apiEndpoints = Api.endpoints(stockQueryService, reservationPersistor)
--- central-query-service/src/main/scala/kafka/ChunkProcessor.scala
+++ central-query-service/src/main/scala/kafka/ChunkProcessor.scala
@@ -11,3 +11,6 @@ type ChunkProcessor[K,V] = Chunk[ConsumerRecord[K,V]] => IO[CommitNow]
 object ChunkProcessor:
   def last[K,V](processor: ConsumerRecord[K,V] => IO[Unit]): ChunkProcessor[K,V] = 
     _.last.traverse(processor).as(CommitNow)
+    
+  def all[K,V](processor: ConsumerRecord[K,V] => IO[Unit]): ChunkProcessor[K,V] = 
+    _.traverse(processor).as(CommitNow)
